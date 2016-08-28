81f0944df9c134c83ad59291447677cf7d3601cd
sliferdragon <sliferdragon@gmail.com>
2016-08-28 06:38:33 -0700

    Init Memory

--- main.rb
+++ main.rb
@@ -1,5 +1,15 @@
 require_relative "input_reader"
-input = InputReader.new()
-input.getRomMemory().each do |i|
-	puts i 
+
+def main()
+	input = InputReader.new()
+	memoryStack =[]
+	memoryStack[0x0000]=0
+	memoryStack[0x1000]=0
+	regsV=[]
+	input.getRomMemory().each_with_index do |instruction,counter|
+		memoryStack[0x200+counter]=instruction
+	end
+	puts "Started"
 end
+
+main()
